662a62964f03cc5c7b782edadc33466bec283ea4
Frances Anastasia <anastafj197@potsdam.edu>
2017-01-26 20:37:31 -0500

    Almost fixed

--- sans.java
+++ sans.java
@@ -24,13 +24,22 @@ public class sans extends FilterReader {
 			}
 			int last = from;
 			for (int i = from; i < from + numChars; i++) {
-
+				if (!commentFlag) {
+					if (buf[i] == '/' && buf[i+1] == '/') 
+						commentFlag = true;
+					else if (buf[i] == '/' && buf[i+1] == '*') 
+						commentFlag = true;
+					else 
+						buf[last++] = buf[i];
+				} else if (buf[i] == '*' && buf[i] == '/') 
+					commentFlag = false; 
 			}
+			numChars = last - from;
 		}
-
-		return 2;
-
+		return numChars;
 	}
+		
+
 
 	public static void main (String [] args) throws FileNotFoundException {
 
