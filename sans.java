2e7e562a9b867e8acffdd44ea8afdb529af28621
frank anastasia <frank anastasia>
2017-01-26 22:24:32 -0500

    cool beans its workin

--- sans.java
+++ sans.java
@@ -26,6 +26,9 @@ public class sans extends FilterReader {
 
 			for (int i = from; i < from + numChars; i++) {
 				if (!commentFlag) {
+					if (buf[i] == '/' && buf[i+1] == '/') {
+						commentFlag = true;
+					}
 					if (buf[i] == '/' && buf[i+1] == '*') {
 						commentFlag = true;
 					} else {
